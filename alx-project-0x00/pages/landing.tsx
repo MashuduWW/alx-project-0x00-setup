869b225d29f8e4e69c5a1ca1cd41354c6c62f23e
Mashudu Molema <mrkaymp@gmail.com>
2025-07-19 20:05:56 +0200

    Added <Card /> from @/components/Card

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,22 +1,29 @@
 import React from "react";
+import Card from "@/components/Card";
 import Button from "@/components/Button";
 
 const LandingPage = () => {
   return (
-    <div className="p-10 space-y-4">
-      <h1 className="text-3xl font-bold">Reusable Buttons</h1>
+    <div className="p-10 space-y-6">
+      <h1 className="text-3xl font-bold">Landing Page</h1>
 
-      <div className="space-x-4">
-        <Button title="Small Button" styles="text-xs rounded-sm" />
-        <Button title="Medium Button" styles="text-base rounded-md" />
-        <Button title="Large Button" styles="text-lg rounded-full" />
+      <Card />
+
+      <div className="space-y-4">
+        <div className="space-x-4">
+          <Button title="Small Button" styles="text-xs rounded-sm" />
+          <Button title="Medium Button" styles="text-base rounded-md" />
+          <Button title="Large Button" styles="text-lg rounded-full" />
+        </div>
+
+        <div className="space-x-4">
+          <Button title="Rounded-sm" styles="rounded-sm" />
+          <Button title="Rounded-md" styles="rounded-md" />
+          <Button title="Rounded-full" styles="rounded-full" />
+        </div>
       </div>
 
-      <div className="space-x-4">
-        <Button title="Rounded-sm" styles="rounded-sm" />
-        <Button title="Rounded-md" styles="rounded-md" />
-        <Button title="Rounded-full" styles="rounded-full" />
-      </div>
+      <Card />
     </div>
   );
 };
